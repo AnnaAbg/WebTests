0f4bc1fa246e95f00561539489246fee25c7ec2e
AnnaAbg <abgariananna8@gmail.com>
2022-06-04 17:21:50 +0300

    Refactored test

---
+++ src/test/java/RefactoredTest.java
@@ -0,0 +1,86 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class RefactoredTest {
+
+    private static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+
+    private void getBottles(StringBuilder lyrics, int number, String btl) {
+        lyrics.append(number).append(btl);
+    }
+
+    private void getNoMore(StringBuilder lyrics, String noMore, String btl) {
+        lyrics.append(noMore).append(btl);
+    }
+
+    private String createLyrics() {
+        final String BOTTLES_WALL_CS = " bottles of beer on the wall, ";
+        final String BOTTLES_DOT_LN = " bottles of beer.\n";
+        final String TAKE = "Take one down and pass it around, ";
+        final String BOTTLES_DOT = " bottle of beer on the wall.";
+        final String GO = "Go to the store and buy some more, ";
+        final String NO_MORE = "No more";
+
+        StringBuilder lyrics = new StringBuilder();
+
+        getBottles(lyrics, 99, BOTTLES_WALL_CS);
+        getBottles(lyrics, 99, BOTTLES_DOT_LN);
+
+        for (int i = 98; i > -1; i--) {
+            lyrics.append(TAKE);
+
+            if (i == 1) {
+                getBottles(lyrics, i, BOTTLES_DOT.replace("s", ""));
+                getBottles(lyrics, i, BOTTLES_WALL_CS.replace("s", ""));
+                getBottles(lyrics, i, BOTTLES_DOT_LN.replace("s", ""));
+
+            } else if (i == 0) {
+                getNoMore(lyrics, NO_MORE.toLowerCase(), BOTTLES_DOT);
+                getNoMore(lyrics, NO_MORE, BOTTLES_WALL_CS);
+                getNoMore(lyrics, NO_MORE.toLowerCase(), BOTTLES_DOT_LN);
+
+            } else {
+                getBottles(lyrics, i, BOTTLES_DOT);
+                getBottles(lyrics, i, BOTTLES_WALL_CS);
+                getBottles(lyrics, i, BOTTLES_DOT_LN);
+
+            }
+        }
+        lyrics.append(GO);
+        getBottles(lyrics, 99, BOTTLES_DOT);
+
+        return lyrics.toString();
+
+    }
+
+    @Test
+    public void testLyricsTest() {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "E:\\chromedriver_win32\\chromedriver.exe";
+        final String expectedResult = createLyrics();
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        By pTags = By.xpath("//div[@id='main']/p");
+        By menuSongLyrics = By.linkText("Song Lyrics");
+
+        driver.get(BASE_URL);
+        driver.findElement(menuSongLyrics).click();
+
+        StringBuilder actualResult = new StringBuilder();
+
+        List<WebElement> pAll = driver.findElements(pTags);
+        for (WebElement p : pAll) {
+            actualResult.append(p.getText());
+        }
+        Assert.assertEquals(actualResult.toString(), expectedResult);
+    }
+}
